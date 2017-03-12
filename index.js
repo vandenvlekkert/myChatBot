d0c22fd685f1afb79d2cccdb902023083c8968cb
Christine van den Vlekkert <xine.vdv@me.com>
2017-03-12 16:45:47 -0700

    setting up PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 3000))
 
 //Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
@@ -19,7 +19,7 @@ app.get('/', function(req, res) {
 //for Facebook verification
 app.get('/webhook/', function(req, res) {
   if(req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-    res.send(req.query['but.challenge'])
+    res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong token')
 })
