9557d77473f827bac241c22d4057881223ce3a10
Christine van den Vlekkert <xine.vdv@me.com>
2017-03-12 17:55:06 -0700

    update bot to speak

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express()
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
 
 app.set('port', (process.env.PORT || 3000))
 
@@ -20,11 +22,27 @@ app.get('/', function(req, res) {
 app.get('/webhook/', function(req, res) {
   if(req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
     res.send(req.query['hub.challenge'])
-  }
+  }else{
   res.send('Error, wrong token')
+  }
 })
-
+//to post data
+app.post('webhook/', function(req, res) {
+  let messaging_events = req.body.entry[0].messaging
+  for(let i = 0; i < messaging_events.length; i++) {
+    let event = req.body.entry[0].messaging[i]
+    let sender = event.sender.id
+    if(event.message && event.message.text) {
+      if(text === 'Generic'){
+        console.log('welcome to mychatbot')
+        //sendGenericMessage(sender)
+      }
+    }
+  }
+})
 //activate the server!
 app.listen(app.get('port'), function() {
   console.log('running on port ', app.get('port'))
+})
+)
 })
